bc238c372d02b10e67ad41a22cd973049831a36d
Shivender-Kun <shivenderkumar761@gmail.com>
2025-07-29 20:47:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <div className="flex gap-8">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+
+        <img src={reactLogo} className="animate-spin" alt="React logo" />
+      </div>
+      <h1 className="text-2xl">Vite + React</h1>
+      <div className="flex flex-col gap-4 items-center">
+        <button
+          className="p-4 border-1 rounded-lg bg-amber-300"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
